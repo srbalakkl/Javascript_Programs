ec52625fb8069b5ce555accbd96e62902ab85384
Sr.Bala <sr.bala.kkl@gmail.com>
2023-08-25 11:49:20 +0530

    doc: finally block in angular

--- Promises.js
+++ Promises.js
@@ -17,6 +17,8 @@ ride.catch(val =>{
 })
 
 ride.finally(val=>{
+    // A finally callback will not receive any argument, since there's no reliable means of determining if the promise was fulfilled or rejected.
+    // This use case is for precisely when you do not care about the rejection reason, or the fulfillment value, and so there's no need to provide it. (mdn)
     console.log('final',val)
 })
 
