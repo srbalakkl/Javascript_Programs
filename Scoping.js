7a5327aeb408eaeb15a08727e5460c7c7583d9a8
bala <sr.bala.kkl@gmail.com>
2025-12-05 10:39:58 +0000

    FIX: Scoping Doc

--- Scoping.js
+++ Scoping.js
@@ -1,8 +1,8 @@
 function foo() {
-    var b = 2;// `var` keyword is global scoped.
+    var b = 2;// `var` keyword is the FUNCTION-SCOPED keyword which mean we can access it from anywhere inside the function.
     // BLOCK SCOPE means anything inside curly braces{}
     if (true) {
-        let c = 3;//<- Here the 'c' variable will only work if you change it from BLOCK SCOPE to GLOBAL SCOPE (i.e, let to var)
+        let c = 3;//<- Here the 'c' variable will only work if you change it from BLOCK SCOPE to FUNCTION SCOPE (i.e, let to var)
     }
 
     console.log(b, c)
